9080bf9c19e8f0c1fd02935f9aa4a52c178aa1ff
Meredith Seaberg <meredithseabergaccount@Merediths-MacBook-Pro.local>
2022-09-28 14:00:48 -0400

    3

--- ping.py
+++ ping.py
@@ -1,5 +1,12 @@
 import os
-from flask import Flask, jsonify 
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
 import time
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,3 +18,11 @@ def index():
     t0 = time.time()
     return str(t0)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
